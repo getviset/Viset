c73bd8c4d4d5a0ae4f968f180ed2b3eb87fa2328
alsi-lawr <177320313+alsi-lawr@users.noreply.github.com>
2026-07-16 10:47:18 +0100

    build: pin validated .NET SDK

--- flake.nix
+++ flake.nix
@@ -1,17 +1,25 @@
 {
   description = "Viset local development shell";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    dotnet-nixpkgs.url = "github:NixOS/nixpkgs/381da955cb03aaa991c7ce15961d3fe50351f895";
+  };
 
   outputs =
-    { nixpkgs, ... }:
+    {
+      dotnet-nixpkgs,
+      nixpkgs,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      dotnetPkgs = import dotnet-nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = pkgs.mkShellNoCC {
-        packages = [ pkgs.dotnet-sdk_10 ];
+        packages = [ dotnetPkgs.dotnet-sdk_10 ];
       };
     };
 }
